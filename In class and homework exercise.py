fe23e53283bf00a691a2afdb1c04666516a66d2e
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-01-16 11:59:10 +0000

    Add files via upload

---
+++ In class and homework exercise.py
@@ -0,0 +1,31 @@
+#2.
+temp = int(input("Enter a temperature: "))
+if temp < 32 :
+    print ("ice")
+elif temp < 112:
+    print ("water")
+else:
+    print ("steam")
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
