fdebdf2c865225ad287ee3577e72d7131cf1f0d7
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-01-20 18:46:41 +0000

    Update In class and homework exercise.py

--- In class and homework exercise.py
+++ In class and homework exercise.py
@@ -1,4 +1,5 @@
 #2.
+print("Number 2: ")
 temp = int(input("Enter a temperature: "))
 if temp < 32 :
     print ("ice")
@@ -7,13 +8,40 @@ elif temp < 112:
 else:
     print ("steam")
         
+#3.
+print("Number 3: ")
+x = 1
+if x > 3:
+    if x > 4:
+        print("A", end=" ")
+    print("B", end=" ")
+elif x < 2:
+    if x > 0:
+        print("C", end=" ")
+    print("D")
 
+#4.
+print("Number 4: ")
+weather = "rain"
+if weather == "sunny":
+    print("wear sunblock")
+elif weather == "snow":
+    print("going skiing")
+else:
+    print(weather)
 
+#5.
+print('Number5: ')
+if int('zero') == 0 :
+    print ("zero")
+elif str(0) == 'zero' :
+    print(0)
+elif str(0) == '0 ' :
+    print (str(0))
+else:
+    print ("none of the above")
 
-
-
-
-
+#6.
 
 
 
